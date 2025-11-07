32d0a44328e5e3f02ac3319fd0d1251318b604a2
Omkar Prajapati <prajapatiomkar9833@gmail.com>
2025-11-07 20:30:11 +0530

    sync.

--- config/database.go
+++ config/database.go
@@ -15,7 +15,7 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
